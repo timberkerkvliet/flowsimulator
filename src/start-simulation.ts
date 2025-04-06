83fc536ac3bd42d4e2eb29821c20e92ed0438818
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-04-06 08:37:51 +0200

    start readme and support query params

--- src/start-simulation.ts
+++ src/start-simulation.ts
@@ -11,64 +11,93 @@ import { Strategy } from "./strategy";
 const button = document.getElementById('startButton');
 
 const teamSizeElement = document.getElementById('teamSize') as HTMLInputElement;
-const randomSeed = document.getElementById('randomSeed') as HTMLInputElement;
+const randomSeedElement = document.getElementById('randomSeed') as HTMLInputElement;
 
-const togetherFactor = document.getElementById('togetherFactor') as HTMLInputElement;
-const speed = document.getElementById('speed') as HTMLInputElement;
+const collaborationEfficiencyElement = document.getElementById('collaborationEfficiency') as HTMLInputElement;
+const speedElement = document.getElementById('speed') as HTMLInputElement;
 
-const maxBatchSize = document.getElementById('maxBatchSize') as HTMLInputElement;
-const wipLimit = document.getElementById('wipLimit') as HTMLInputElement;
-
+const batchSizeElement = document.getElementById('batchSize') as HTMLInputElement;
+const wipLimitElement = document.getElementById('wipLimit') as HTMLInputElement;
 
 
 class SimulationState {
     public static runner: SimulationRunner | undefined;
 }
 
-if (button) {
-    button.addEventListener('click', async () => {
-        button.textContent = "Update";
-        teamSizeElement.disabled = true;
-        randomSeed.disabled = true;
+function valuesFromUrl() {
+    const queryString = window.location.search;
+    const params: Record<string, string> = {};
+    const urlParams = new URLSearchParams(queryString);
+    urlParams.forEach((value, key) => {
+        params[key] = value;
+      });
 
-        const batchOfWorkFactory = new UnitOfWorkFactory(
-            {
-                randomSeed: seedrandom(randomSeed.value),
-                togetherFactor: parseFloat(togetherFactor.value)/10
-            }
-        );
-
-        const strategy = new Strategy(
-            {
-                batchSize: PositiveInteger.fromNumber(parseFloat(maxBatchSize.value)),
-                wipLimit: PositiveInteger.fromNumber(parseFloat(wipLimit.value))
-            }
-        )
-
-        const teamSize = PositiveInteger.fromNumber(parseFloat(teamSizeElement.value));
-
-        const sleepTime = 5000/parseFloat(speed.value)
-        
-        if (SimulationState.runner === undefined) {
-            const team = Team.new(
-                Backlog.newBacklog(batchOfWorkFactory, PositiveInteger.fromNumber(30), teamSize),
-                strategy,
-                teamSize
-            )
-
-            const runner = new SimulationRunner(
-                team,
-                new Renderer(),
-                sleepTime
-            );
-
-            SimulationState.runner = runner;
-            await runner.run();
-        } else {
-            SimulationState.runner.updateWorkFactory(batchOfWorkFactory);
-            SimulationState.runner.updateStrategy(strategy);
-            SimulationState.runner.updateTeamSize(teamSize);
-            SimulationState.runner.updateSleepTime(sleepTime);
-        }
-    });
+    if (params["teamSize"]) {
+        teamSizeElement.value = params["teamSize"];
+    }
+    if (params["randomSeed"]) {
+        randomSeedElement.value = params["randomSeed"];
+    }
+    if (params["collaborationEfficiency"]) {
+        collaborationEfficiencyElement.value = params["collaborationEfficiency"];
+    }
+    if (params["speed"]) {
+        speedElement.value = params["speed"];
+    }
+    if (params["batchSize"]) {
+        batchSizeElement.value = params["batchSize"];
+    }
+    if (params["wipLimit"]) {
+        wipLimitElement.value = params["wipLimit"];
+    }
 }
+
+async function clickButton() {
+    button.textContent = "Update";
+    teamSizeElement.disabled = true;
+    randomSeedElement.disabled = true;
+
+    const batchOfWorkFactory = new UnitOfWorkFactory(
+        {
+            randomSeed: seedrandom(randomSeedElement.value),
+            togetherFactor: parseFloat(collaborationEfficiencyElement.value)/10
+        }
+    );
+
+    const strategy = new Strategy(
+        {
+            batchSize: PositiveInteger.fromNumber(parseFloat(batchSizeElement.value)),
+            wipLimit: PositiveInteger.fromNumber(parseFloat(wipLimitElement.value))
+        }
+    )
+
+    const teamSize = PositiveInteger.fromNumber(parseFloat(teamSizeElement.value));
+
+    const sleepTime = 5000/parseFloat(speedElement.value)
+    
+    if (SimulationState.runner === undefined) {
+        const team = Team.new(
+            Backlog.newBacklog(batchOfWorkFactory, PositiveInteger.fromNumber(30), teamSize),
+            strategy,
+            teamSize
+        )
+    
+        const runner = new SimulationRunner(
+            team,
+            new Renderer(),
+            sleepTime
+        );
+    
+        SimulationState.runner = runner;
+        await runner.run();
+    } else {
+        SimulationState.runner.updateWorkFactory(batchOfWorkFactory);
+        SimulationState.runner.updateStrategy(strategy);
+        SimulationState.runner.updateTeamSize(teamSize);
+        SimulationState.runner.updateSleepTime(sleepTime);
+    }
+}
+
+button.addEventListener('click', clickButton);
+
+valuesFromUrl();
