73fa5d4903f000540cc37aaf47e356fee196abd9
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-07-16 08:26:35 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '',
   build: {
+    rollupOptions: {
+      input: {
+        flowsim: 'src/flowsim/flowsim.html'
+      },
+    },
     assetsDir: 'assets',
   },
 });
